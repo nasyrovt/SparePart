f2fed4ba853bff60b21f3b1d5e31a29bcf5d96aa
Nasyrov Takhir <tahanasyrov@gmail.com>
2024-02-12 13:08:27 +0100

    Init

---
+++ Source/SparePart/SparePart.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SparePart : ModuleRules
+{
+	public SparePart(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
---
+++ Source/SparePart/SparePartGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SparePartGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASparePartGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASparePartGameMode();
+};
+
+
+
